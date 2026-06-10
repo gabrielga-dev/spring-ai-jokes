755d1df23491d2c7af6d635b12d0b8267e76cd69
gabrielga-dev <dealmeida.gabriel.d@gmail.com>
2026-06-10 14:39:36 -0300

    first commit

---
+++ src/main/java/br/com/gabrielga_dev/spring_ai_jokes/SpringAiJokesApplication.java
@@ -0,0 +1,13 @@
+package br.com.gabrielga_dev.spring_ai_jokes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiJokesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiJokesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gabrielga_dev/spring_ai_jokes/SpringAiJokesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gabrielga_dev.spring_ai_jokes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiJokesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
